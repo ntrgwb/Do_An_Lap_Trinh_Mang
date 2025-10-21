426bc8b91408006cb580dcfa32445d91e4ee2c03
Truong <ntruonglovek41@gmail.com>
2025-10-21 10:41:20 +0700

    Add WinForms project

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtName = new TextBox();
+            btnSave = new Button();
+            lstNames = new ListBox();
+            SuspendLayout();
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(250, 88);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(125, 27);
+            txtName.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(267, 160);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 29);
+            btnSave.TabIndex = 1;
+            btnSave.Text = "Lưu vào db";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // lstNames
+            // 
+            lstNames.FormattingEnabled = true;
+            lstNames.Location = new Point(250, 222);
+            lstNames.Name = "lstNames";
+            lstNames.Size = new Size(150, 104);
+            lstNames.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstNames);
+            Controls.Add(btnSave);
+            Controls.Add(txtName);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtName;
+        private Button btnSave;
+        private ListBox lstNames;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        private FirebaseClient firebase;
+        public Form1()
+        {
+            InitializeComponent();
+            firebase = new FirebaseClient("https://thuyettrinhdemo-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        }
+
+        private async void btnSave_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text.Trim();
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("Nhập tên trước đã!");
+                return;
+            }
+
+            // Ghi dữ liệu
+            await firebase.Child("names").PostAsync(new { name });
+
+            MessageBox.Show($"Đã lưu {name} lên Firebase!");
+            txtName.Clear();
+
+            await LoadData();
+        }
+
+        private async Task LoadData()
+        {
+            lstNames.Items.Clear();
+
+            var data = await firebase.Child("names").OnceAsync<object>();
+            foreach (var item in data)
+            {
+                dynamic d = item.Object;
+                lstNames.Items.Add(d.name.ToString());
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await LoadData();
+        }
+    }
+}
